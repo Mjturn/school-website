b6b9e330b0409889d6673d9d8919c7e808ad3d4c
Mjturn <Mjturn@outlook.com>
2024-06-28 18:39:26 -0400

    connect to database

---
+++ src/test/java/com/mjturn/school_website/SchoolWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjturn.school_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
